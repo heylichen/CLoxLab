9a19a032db895cdb74c6b51ae0ca31ab23e0ea8e
heylichen <heylichen@qq.com>
2022-12-05 20:29:03 +0800

    ch14 chunks of Bytecode

---
+++ value.h
@@ -0,0 +1,22 @@
+//
+// Created by lc on 2022/12/5.
+//
+
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+#endif
