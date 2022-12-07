d4bb7c2640e7bf963d9976f3949b9f1e00db26d1
heylichen <heylichen@qq.com>
2022-12-07 21:34:42 +0800

    ch15 A Virtual Machine

---
+++ vm.h
@@ -0,0 +1,33 @@
+//
+// Created by lc on 2022/12/7.
+//
+
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+#include "value.h"
+#define STACK_MAX 256
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+
+typedef struct {
+    Chunk* chunk;
+    //instruction pointer
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
+
+#endif
